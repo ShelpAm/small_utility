06fdb28bab3e1d555da1c376a54cb0d057b42e61
small_sheep <1178550325@qq.com>
2022-05-16 10:33:36 +0800

    Removed all the 'xmake.lua'.

--- src/xmake.lua
+++
@@ -1,8 +0,0 @@
-target("small_utility")
-    set_kind("static")
-
-    set_objectdir("$(buildir)/obj/")
-    add_includedirs("$(projectdir)/include/")
-    add_linkdirs("$(projectdir)/lib/")
-
-    add_files("**.cc")
