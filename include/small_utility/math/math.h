0857cf6e72ebb7aacc08a696763fedbc1ed9bb5f
small_sheep <1178550325@qq.com>
2022-07-04 22:06:42 +0800

    Removed math from this library.

--- include/small_utility/math/math.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMAND_AND_DOMINATE_SRC_MATH_MATH_H_
-#define COMMAND_AND_DOMINATE_SRC_MATH_MATH_H_
-
-#include "small_utility/math/vector/vector.h"
-#include "small_utility/math/matrix/matrix.h"
-
-#include "small_utility/math/type_ptr.h"
-
-#endif // !COMMAND_AND_DOMINATE_SRC_MATH_MATH_H_
--- include/small_utility/math/vector/vector.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMAND_AND_DOMINATE_SRC_MATH_VECTOR_VECTOR_H_
-#define COMMAND_AND_DOMINATE_SRC_MATH_VECTOR_VECTOR_H_
-
-#include "small_utility/math/vector/vector2d.h"
-#include "small_utility/math/vector/vector3d.h"
-#include "small_utility/math/vector/vector4d.h"
-
-#endif // !COMMAND_AND_DOMINATE_SRC_MATH_VECTOR_VECTOR_H_
